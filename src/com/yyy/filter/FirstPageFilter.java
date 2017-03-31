8512c1b1e43679582640d55fc626802ba0e26338
waixiaoyu <yaoyu_best@163.com>
2017-03-31 17:43:20 -0400

    add filter for first page

---
+++ src/com/yyy/filter/FirstPageFilter.java
@@ -0,0 +1,58 @@
+package com.yyy.filter;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class FirstPageFilter
+ */
+@WebFilter(filterName = "firstPageFilter", urlPatterns = "/*")
+public class FirstPageFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public FirstPageFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		String path = req.getServletPath();
+		if ("/".equals(path)) {
+			request.getRequestDispatcher("fuzz.jsp").forward(request, response);
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
